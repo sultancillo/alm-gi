bc8183f7d1377748445183b46e27cacbfe32c3c0
Mario Valencia <mariovalencia@gmail.com>
2025-07-02 08:32:15 -0600

    Compound components.

---
+++ web/themes/custom/agi/components/00-base/01-colors/colors.stories.js
@@ -0,0 +1,13 @@
+import colors from "./colors.twig";
+
+import colorsData from "./colors.yml";
+import "./colors";
+
+/**
+ * Storybook Definition.
+ */
+export default {
+  title: "Base/Colors",
+};
+
+export const Palettes = () => colors(colorsData);
